c292404700785a343d362dca6804b012ab13b74b
luiz fernando <luiz123jfmg@gmail.com>
2021-03-31 11:02:56 -0300

    Listando categorias

---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { API_CONFIG } from "../../config/api.config";
+import { CategoriaDTO } from "../../models/categoria.dto";
+import { Observable } from "rxjs/Rx";
+
+@Injectable()
+export class CategoriaService {
+
+    constructor(public http: HttpClient){
+
+    }
+
+    //Função tipada retornando uma lista de um objeto categoria
+    findAll() : Observable<CategoriaDTO[]> {
+        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+    }
+}
