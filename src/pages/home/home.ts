d2f1a419aec60798fd249dff580ea8ca2fab8fd2
luiz fernando <luiz123jfmg@gmail.com>
2021-04-01 14:07:19 -0300

    Pegando os dados do formulário de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()
 
@@ -8,6 +9,11 @@ import { IonicPage, MenuController, NavController } from 'ionic-angular';
   templateUrl: 'home.html' //referencia para qual arquivo está controlando
 })
 export class HomePage {
+
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
@@ -23,6 +29,7 @@ export class HomePage {
 
   //FAZ A NAVEGAÇÃO DA HOME PAGE PARA A CATEGORIA PAGE
   login(){
+    console.log(this.creds)
     this.navCtrl.setRoot('CategoriasPage');
   }
 
